e395f8de3d0735cd13439f63d81d3855ddc3e1c1
Abhinav Goyal <abhinav.goyal@live.com>
2017-11-17 01:54:46 +0800

    remove_dup

---
+++ code_list/remove_dup.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <list>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+void print(list <int> linked_list, list<int>::iterator itr) {
+    for (itr= linked_list.begin(); itr!= linked_list.end(); ++itr) {
+        cout << *itr << endl;
+    }
+}
+
+int main() {
+    list <int> linked_list;
+    unordered_map <int, int> hashmap;
+    list <int>::iterator itr;
+    // creating a linked_list with duplicates
+    for (int i = 0; i < 10; i++) {
+        linked_list.push_back(i);
+        linked_list.push_back(i+1);
+    }
+    // display
+    print(linked_list, itr);
+    for (itr = linked_list.begin(); itr != linked_list.end(); ++itr) {
+        if (hashmap[*itr])
+            linked_list.erase(itr);
+        else 
+            hashmap[*itr]++;
+    }
+    print(linked_list, itr);
+    return 0;
+}
