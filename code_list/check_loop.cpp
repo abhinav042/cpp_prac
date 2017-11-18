3ddb11db33d91031b5467e156ff1bca90c2acafd
Abhinav Goyal <abhinavgoyal@Abhinavs-MacBook-Pro.local>
2017-11-19 07:49:17 +0800

    more ex

---
+++ code_list/check_loop.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <list>
+#include <iterator>
+
+using namespace std;
+
+void print(list <int> linked_list, list<int>::iterator itr) {
+    for (itr= linked_list.begin(); itr!= linked_list.end(); ++itr) {
+        cout << *itr << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    list <int> linked_list({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+    list <int>::iterator itr;
+    itr = linked_list.end();
+    linked_list.begin() = itr;
+    if (linked_list.end() == linked_list.begin()) cout << "it works" << endl;
+    else cout << "it doesnt";
+    print(linked_list, itr);
+    return 0;
+}
