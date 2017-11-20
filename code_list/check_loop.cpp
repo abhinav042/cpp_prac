b17d4a1c66ae5ddb2f637fe0a42db0f449cc828c
Abhinav Goyal <abhinav.goyal@live.com>
2017-11-20 23:27:47 +0800

    files

--- code_list/check_loop.cpp
+++ code_list/check_loop.cpp
@@ -4,6 +4,11 @@
 
 using namespace std;
 
+struct Node {
+    int data;
+    struct Node* next;
+};
+
 void print(list <int> linked_list, list<int>::iterator itr) {
     for (itr= linked_list.begin(); itr!= linked_list.end(); ++itr) {
         cout << *itr << " ";
@@ -11,17 +16,34 @@ void print(list <int> linked_list, list<int>::iterator itr) {
     cout << endl;
 }
 
+bool has_cycle(Node* head) {
+    // Complete this function
+    // Do not write the main method
+    
+    
+    if (head == nullptr) {
+        return false;
+    }
+    
+    Node* slow_ptr = new Node();
+    Node* fast_ptr = new Node();
+    
+    slow_ptr = head;
+    fast_ptr = head;
+    
+    while(fast_ptr!=nullptr && fast_ptr->next!=nullptr) {
+        slow_ptr = slow_ptr->next;
+        fast_ptr = (fast_ptr->next)->next;
+        if (slow_ptr == fast_ptr) {
+            return true;
+        }
+    }
+    
+    return false;
+    
+}
+
 int main() {
-    list <int> linked_list({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
-    list <int>::iterator itr;
-    itr = linked_list.end();
-    linked_list.begin() = itr;
-    if (linked_list.end() == linked_list.begin()) cout << "it works" << endl;
-    else cout << "it doesnt";
-    print(linked_list, itr);
-<<<<<<< HEAD
-    cout << "under construction";
-=======
->>>>>>> 527537de0412a0e171272b33efb7fe0d69b219e9
+    // handled via the buffer
     return 0;
 }
