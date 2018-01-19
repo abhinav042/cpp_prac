cf62c50a887bed8a27f285fbec2340e21ca5136e
Abhinav Goyal <abhinav.goyal@live.com>
2018-01-19 01:22:36 -0800

    update

--- hacker_rank/balance_brackets.cpp
+++ hacker_rank/balance_brackets.cpp
@@ -23,9 +23,32 @@
 
 using namespace std;
 
+bool is_matched(char ch1, char ch2) {
+    if ((ch1 == '(' && ch2 == ')') || (ch1 == '{' && ch2 == '}') || (ch1 == '[' && ch2 == ']'))
+        return true;
+    else 
+        return false;
+}
+
 bool is_balanced(string expression) {
-    
-    return false;
+    stack <char> s;
+    for (auto letter : expression) {
+        if (letter == '{' || letter == '(' || letter == '[') {
+            s.push(letter);
+        }
+        else {
+            // cout << "letter is" << letter << " top stack is " << s.top() << endl;
+            if (!s.empty() && is_matched(s.top(), letter))
+                s.pop();
+            else 
+                return false;
+        }
+        // cout << s.top() << endl;
+    }
+    if (s.empty())
+        return true;
+    else 
+        return false;
 }
 
 int main(){
