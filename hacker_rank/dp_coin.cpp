9c0634bbe41a6d4e99bdeaf7209d67005031a52a
Abhinav Goyal <abhinav.goyal@live.com>
2018-01-30 12:37:58 -0800

    add trivial problems

--- hacker_rank/dp_coin.cpp
+++ hacker_rank/dp_coin.cpp
@@ -34,22 +34,18 @@ long long make_change(vector<int> coins, int money, unordered_map <string, long>
     long ways = 0;
     int coin_val = coins[index];
     key = to_string(money) + '-' + to_string(coin_val);
-    // cout << "the key is : " << key << endl;
+   
     if (cache.find(key) != cache.end()) {
-        // cout << "the cache is being entered..." << endl;
+        
         return cache[key];
     }
     for (int i = 0; i * coin_val <= money; i++) {
-        // cout << "weee i went here..." << endl;
-        // cout << endl;
+        
         int remaining_money = money - i * coin_val;
         ways += make_change (coins, remaining_money, cache, index + 1);
     }
     cache[key] = ways;
-    // auto itr = cache.find(key);
-    // if (itr != cache.end()) {
-    //     cout << "*itr" << endl;
-    // }
+    
     return ways;
 }
 
